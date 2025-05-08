14950084ace5aa731a45ff9ef31b87997322614a
AnhuaW <71238522+AnhuaW@users.noreply.github.com>
2025-05-08 00:31:51 -0400

    updated audio sequence player

--- Assets/PlayAudioSequence.cs
+++ Assets/PlayAudioSequence.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayAudioSequence : MonoBehaviour
 {
+    public List<AudioClip> audioClips = new List<AudioClip>();
+    UnityEvent onPause = new UnityEvent();
+    [SerializeField] AudioSource _audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _audioSource = gameObject.GetComponent<AudioSource>();
+        StartCoroutine(PlayWithDelay());
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    IEnumerator PlayWithDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        foreach (AudioClip clip in audioClips)
+        {
+            _audioSource.PlayOneShot(clip);
+            yield return new WaitForSeconds(clip.length);
+            if (clip.name == "question")
+            {
+                onPause.Invoke();
+            }
+        }
     }
 }
